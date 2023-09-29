bc439791bcedadeaaa710eebca3b21bef180b426
Alex Rofes <alexrofes1992@gmail.com>
2023-09-29 19:58:31 +0200

    sql

--- api/schema.sql
+++ api/schema.sql
@@ -7,7 +7,6 @@ CREATE TABLE users (
     name VARCHAR(255),
     email VARCHAR(100) NOT NULL,
     password VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255)
 );
 
 CREATE TABLE todos (
@@ -25,3 +24,23 @@ CREATE TABLE shared_todos (
     FOREIGN KEY (todo_id) REFERENCES todos(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 )
+
+-- Insert two users
+
+INSERT INTO users (name, email, password) VALUES ('John Doe', 'john@gmail.com', '123456');
+INSERT INTO users (name, email, password) VALUES ('Alex Do', 'alex@gmail.com', '1111111');
+
+--Insert todos into todos TABLE
+INSERT INTO todos (title, user_id) 
+VALUES
+('Go for a morning run', 1),
+('Work on projecto presentacion', 1),
+('Go grocery shopping', 1),
+('Read 30 pages of book', 1),
+('Ride bike to the park', 1),
+('Cook dinner for family', 1),
+('Practice yoga', 1),
+('Listen to podcast', 1),
+('Clean theu house', 1),
+('Get 8 hours to sleep', 1);
+ 
