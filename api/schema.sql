1a2731cdceaccbe1cfdb23ff144de52709f724f7
Alex Rofes <alexrofes1992@gmail.com>
2023-09-29 19:26:24 +0200

    a

--- api/schema.sql
+++ api/schema.sql
@@ -1,1 +1,27 @@
 CREATE DATABASE TaskAplication;
+
+USE TaskAplication;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255)
+);
+
+CREATE TABLE todos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    completed BOOLEAN DEFAULT false,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE shared_todos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    todo_id INT,
+    user_id INT,
+    FOREIGN KEY (todo_id) REFERENCES todos(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+)
