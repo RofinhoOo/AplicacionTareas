44c515519a7904f7df62d5cf04396bc075316e70
Alex Rofes <alexrofes1992@gmail.com>
2023-10-08 12:32:49 +0200

    hola

--- api/schema.sql
+++ api/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     email VARCHAR(100) NOT NULL,
-    password VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE todos (
@@ -21,8 +21,10 @@ CREATE TABLE shared_todos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     todo_id INT,
     user_id INT,
-    FOREIGN KEY (todo_id) REFERENCES todos(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    shared_user_id INT,
+    FOREIGN KEY (todo_id) REFERENCES todos(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (shared_user_id) REFERENCES users(id) ON DELETE CASCADE
 )
 
 -- Insert two users
@@ -43,4 +45,13 @@ VALUES
 ('Listen to podcast', 1),
 ('Clean theu house', 1),
 ('Get 8 hours to sleep', 1);
+
+INSERT INTO shared_todos (todo_id, user_id, shared_with_id) 
+VALUES (1, 1, 2);
+
+SELECT todos.*, shared_todos.shared_with_id 
+FROM todos
+LEFT JOIN shared_todos ON todos.id = shared_todos.todo_id
+WHERE todos.user_id = [user_id] OR shared_todos.shared_with_id = [user_id];
+```
  
